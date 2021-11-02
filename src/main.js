27d0cc4ad3e1ce58a58b4d98364bb018020ab809
Casper <lcasper@mac.com>
2021-11-02 12:40:07 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import store from './store'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
+
+Vue.use(BootstrapVue)
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
+
 new Vue({
-  render: h => h(App)
+//add this line to your main.js file
+store,
+render: h => h(App)
 }).$mount('#app')
